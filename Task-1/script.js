b942b5d0b68828e46fa1cad18fb4b2c05cce441e
Jahanzaib Siddiqui <jahanzaibstudy559@gmail.com>
2025-07-24 13:21:32 +0500

    Delete Task-1 directory

--- Task-1/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('togglebtn').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    const togglebtn = document.getElementById('togglebtn');
-    const content = document.querySelector('.content');
-    
-    sidebar.classList.toggle('closed');
-    content.classList.toggle('sidebar-closed');
-    togglebtn.classList.toggle('sidebar-closed'); // Adjusts the button position
-});
